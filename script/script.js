c6684ec12aece23232412851d23eaf2a5d5e7802
camioliveira24 <84456300+camioliveira24@users.noreply.github.com>
2021-09-29 12:22:09 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,31 @@
+//header
+
+
+//hiro
+
+
+
+//lançamentos
+
+
+
+//acessorios
+
+
+
+//banner horizon
+
+
+//carrossel novidades
+
+
+//banner plus
+
+
+//promoções
+
+
+//redes sociais
+
+
+//footer
