402d6a17b44d4b225e6400867df66359991c61c7
Jean Carlos <79324279+JeCarlos7713@users.noreply.github.com>
2021-10-04 10:50:39 -0300

    Evento scroll menu

---
+++ js/top.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    var offset = document.getElementById('menu').offsetTop; //Distância/Deslocamento do Menu para o topo
+    //alert(offset)
+    var menu = document.getElementById('menu'); //Simplificar o código
+
+    document.addEventListener('scroll', function() { //Listener = escuta o site meu filho, acompanhamento do site. Aqui ele acompanha o scroll e chama a function
+        if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) { //Se o scroll for maior que o offset
+            menu.style.position = 'fixed';
+            menu.style.top = '0';
+            menu.style.transition = '0.7s';
+        } else {
+            menu.style.position = 'initial'; //Voltando ao padrão
+        }
+    });
+}
+
+
